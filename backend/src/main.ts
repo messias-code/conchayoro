b0ed82256853ce7dc76060fc11e8a6f51a8d5174
messias-code <messias.devops@gmail.com>
2025-06-12 04:05:06 +0000

    update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  const PORT = process.env.PORT || 8080;
+  await app.listen(PORT);  // Muda para 8080
 }
 bootstrap();
